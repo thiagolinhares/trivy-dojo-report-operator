7e6dde3f8a7f53415a87479c00769220a858e11f
Thiago Linhares <z.linha@gmail.com>
2023-09-11 18:05:12 -0300

    extra metadata fields

--- src/handlers.py
+++ src/handlers.py
@@ -3,6 +3,7 @@ from io import BytesIO
 import requests
 from requests.exceptions import HTTPError
 import kopf
+from datetime import datetime
 
 import settings
 
@@ -62,6 +63,32 @@ def send_to_dojo(body, meta, logger, **_):
     json_file: BytesIO = BytesIO(json_string.encode("utf-8"))
     report_file: dict = {"file": ("report.json", json_file)}
 
+    # Given timestamp 2023-09-11T06:36:16Z, we want to extract the date and month as follows:
+    # scan_date = 2023-09-11
+    # scan_month = September
+    scan_date = datetime.strptime(meta["creationTimestamp"], '%Y-%m-%dT%H:%M:%SZ').strftime('%Y-%m-%d')
+    scan_month = datetime.strptime(meta["creationTimestamp"], '%Y-%m-%dT%H:%M:%SZ').strftime('%B')
+    # Define few variables for the defectdojo api
+    namespace = f'{meta["labels"]["trivy-operator.resource.namespace"]}'
+    kind = f'{meta["labels"]["trivy-operator.resource.kind"]}'
+    container = f'{meta["labels"]["trivy-operator.container.name"]}'
+    # Trim the name for ReplicaSets
+    if kind == "ReplicaSet":
+        name = "-".join(input.operatorObject.metadata.labels["trivy-operator.resource.name"].split("-")[:-1])
+    else:
+        name = f'{meta["labels"]["trivy-operator.resource.name"]}'
+
+    image_name = f'{body["report"]["registry"]["server"]}/{body["report"]["artifact"]["repository"]}'
+    image_version = f'{body["report"]["arficat"]["tag"]}'
+    image_full_name = f'{image_name}:{image_version}'
+    image_digest = f'{body["report"]["artifact"]["digest"]}'
+
+    # Service name should be constructed like this:
+    # default__replicaset__defectdojo-django__nginx__defectdojo/defectdojo-nginx
+    # <namespace>__<kind>__<name>__<container>__<image>
+    # <name> for replicasets are trimmed the last section
+    service = f'{namespace}__{kind}__{name}__{container}__{body["report"]["artifact"]["repository"]}'
+
     headers: dict = {
         "Authorization": "Token " + settings.DEFECT_DOJO_API_KEY,
         "Accept": "application/json",
@@ -70,22 +97,28 @@ def send_to_dojo(body, meta, logger, **_):
     data: dict = {
         "active": settings.DEFECT_DOJO_ACTIVE,
         "verified": settings.DEFECT_DOJO_VERIFIED,
-        "close_old_findings": settings.DEFECT_DOJO_CLOSE_OLD_FINDINGS,
+        "scan_date": scan_date,
+        "close_old_findings": settings.DEFECT_DOJO_CLOSE_OLD_FINDINGS_PRODUCT_SCOPE,
         "close_old_findings_product_scope": settings.DEFECT_DOJO_CLOSE_OLD_FINDINGS_PRODUCT_SCOPE,
-        "push_to_jira": settings.DEFECT_DOJO_PUSH_TO_JIRA,
         "minimum_severity": settings.DEFECT_DOJO_MINIMUM_SEVERITY,
         "auto_create_context": settings.DEFECT_DOJO_AUTO_CREATE_CONTEXT,
         "deduplication_on_engagement": settings.DEFECT_DOJO_DEDUPLICATION_ON_ENGAGEMENT,
+        "do_not_reactivate": settings.DEFECT_DOJO_DO_NOT_REACTIVATE,
         "scan_type": "Trivy Operator Scan",
-        "engagement_name": meta["creationTimestamp"],
-        "product_name": body["report"]["artifact"]["repository"],
+        "engagement_name": f'FedRamp Audit - {scan_month}',
+        "environment": settings.DEFECT_DOJO_ENVIRONMENT,
+        "product_name": settings.DEFECT_DOJO_PRODUCT_NAME,
         "product_type_name": settings.DEFECT_DOJO_PRODUCT_TYPE_NAME,
-        "test_title": settings.DEFECT_DOJO_TEST_TITLE,
+        "group_by": settings.DEFECT_DOJO_GROUP_BY,
+        "test_title": meta["name"],
+        "service": service,
+        "version": image_version,
+        "tags": f'image_digest={image_digest}',
     }
 
     try:
         response: requests.Response = requests.post(
-            settings.DEFECT_DOJO_URL + "/api/v2/import-scan/",
+            settings.DEFECT_DOJO_URL + "/api/v2/reimport-scan/",
             headers=headers,
             data=data,
             files=report_file,
